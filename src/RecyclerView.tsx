9f0d236eab6931b1e64fdf86d8a99abb54287ba5
DaeWon9 <qkreodnjs97@naver.com>
2024-01-14 17:58:07 +0900

    [ADD] RecyclerView

---
+++ src/RecyclerView.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FixedSizeList as List } from "react-window";
+
+interface Base {
+    id?: string | number;
+}
+
+interface Props<T> {
+    itemCount: number;
+    itemSize: number;
+    items: (T & Base)[];
+    renderItem: (item: T, index: number) => React.ReactNode;
+    keyExtractor?: (item: T) => string;
+    initialNumToRender?: number;
+}
+
+function RecyclerView<T>({ itemCount, items, renderItem, keyExtractor, initialNumToRender, itemSize }: Props<T>) {
+    return (
+        <List
+            height={itemSize * ((initialNumToRender || itemCount) - 1)}
+            overscanCount={0}
+            itemCount={itemCount}
+            itemSize={itemSize}
+            width="100%"
+        >
+            {({ index, style }) => {
+                const item = items[index];
+                return (
+                    <div style={style} key={keyExtractor ? keyExtractor(item) : item.id}>
+                        {renderItem(item, index)}
+                    </div>
+                );
+            }}
+        </List>
+    );
+}
+
+export default RecyclerView;
